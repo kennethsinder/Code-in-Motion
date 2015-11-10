2f2817adab1d239f5ec749c5c2f10e0f5681d35d
Kenneth Sinder <kennethsinder@outlook.com>
2015-11-10 13:58:40 -0500

    Added search functionality for blogs

--- database/migrations/2015_11_08_143447_create_tags_table.php
+++ database/migrations/2015_11_08_143447_create_tags_table.php
@@ -35,7 +35,7 @@ class CreateTagsTable extends Migration
      */
     public function down()
     {
-        Schema::drop('tags');
         Schema::drop('blog_tag');
+        Schema::drop('tags');
     }
 }
